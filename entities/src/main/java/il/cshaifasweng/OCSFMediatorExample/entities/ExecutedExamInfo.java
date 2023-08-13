b674681a0b6493575ad0b3ca654f656566df39a3
hajrajaa <129968202+hajrajaa@users.noreply.github.com>
2023-08-13 17:50:04 +0300

    exec exam update

--- entities/src/main/java/il/cshaifasweng/OCSFMediatorExample/entities/ExecutedExamInfo.java
+++ entities/src/main/java/il/cshaifasweng/OCSFMediatorExample/entities/ExecutedExamInfo.java
@@ -9,15 +9,24 @@ import java.io.Serializable;
 @Table(name="executedexamsinfo")
 public class ExecutedExamInfo implements Serializable {
 
+
+    public enum ExamType{
+        Virtual,Manual;
+    }
+
     @Id
     private int id ;
 
-    private String code;
+
+    private int code;
     private String password;
 
     private int overtime;
     private double average;
     private double median;
+
+    private ExamType type;
+
 
     private int[] hist;
 
@@ -28,26 +37,31 @@ public class ExecutedExamInfo implements Serializable {
 //    @JoinColumn(name = "teacher_id")
 //    private Teacher executingTeacher;
 
-    public ExecutedExamInfo(String code, String password, double average, double median) {
+    public ExecutedExamInfo(int code, String password, double average, double median,ExamType type) {
         this.code = code;
         this.password = password;
         this.overtime = 0;
         this.average = average;
         this.median = median;
+        this.type=type;
         this.hist = new int[10];
         this.executedExamList = new ArrayList<ExecutedExam>();
+    }
+
+    public  ExecutedExamInfo(int code,String password,ExamType type)
+    {
+        this.code=code;
+        this.password=password;
+        this.type=type;
+        this.hist = new int[10];
+        this.executedExamList = new ArrayList<ExecutedExam>();
+
     }
 
     public ExecutedExamInfo() {
     }
 
-    public String getCode() {
-        return code;
-    }
 
-    public void setCode(String code) {
-        this.code = code;
-    }
 
     public void setOvertime (int d) { this.overtime = d;}
 
@@ -75,7 +89,24 @@ public class ExecutedExamInfo implements Serializable {
         this.median = median;
     }
 
-   // public int[] getHist() {
+    public ExamType getType() {
+        return type;
+    }
+
+    public void setType(ExamType type) {
+        this.type = type;
+    }
+
+    public int getCode() {
+        return code;
+    }
+
+    public void setCode(int code) {
+        this.code = code;
+    }
+
+
+    // public int[] getHist() {
        // return hist;
     //}
 
